2c79835ea5cec960d8d83b4f86a968d71edfa639
unknown <slavik3333>
2019-05-09 14:22:07 +0300

    kolejne gowno

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,46 @@
-import hello from './hello';
-//import '_'from'lodash';
 import 'bootstrap'
 
-$(document).ready(
-    function (){
-        hello();
+$(document).ready(function (){
+        $('.p-share button').click(function(){
+            $('.share-wrapper').toggle(1000, function () {
+                if ($(this).is(":visible") == true) {
+                    $(this).css('display', 'flex');
+                }
+                else {
+                    $(this).css('display', 'none');
+                }
+            });
+        });
+        $('.next-button, .previous-button').click(function () {
+            if ($('#p-blog1').is(":visible") == true) {
+                $('#p-blog1').hide();
+                $('#p-blog2').show();
+            }
+            else {
+                $('#p-blog1').show();
+                $('#p-blog2').hide();
+
+            }
+        });
+        $('.searchIcon i').click(function(){
+            $('.searchInput').toggle(1000, function () {
+                if ($(this).is(":visible") == true) {
+                    $(this).css('display', 'flex');
+                }
+                else {
+                    $(this).css('display', 'none');
+                }
+            });
+        });
+        $('.dropdown-menu1 i').click(function(){
+            $('.menu').toggle(1000, function () {
+                if ($(this).is(":visible") == true) {
+                    $(this).css('display', 'flex');
+                }
+                else {
+                    $(this).css('display', 'none');
+                }
+            });
+        });
     }
 );
